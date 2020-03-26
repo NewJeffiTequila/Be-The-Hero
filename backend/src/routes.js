90e8778a617c13bae0c27edf9da362c9813667d6
jeffersoncostaIfma <jeffersoncosta@acad.ifma.edu.br>
2020-03-26 14:04:52 -0300

    banco de dados

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const routes = express.Router()
+
+routes.post('/ongs',(request , response)=>{
+    const data = request.body;
+    console.log(data);
+    return response.json({
+
+    });
+});
+
+module.exports = routes;
